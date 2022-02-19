a7ebe427dd2974c4f9f3df464a1dafbebf713507
Lozye <Lozye@qq.com>
2022-02-19 09:44:33 +0800

    v20220219

--- configs/WTF/Kui_Nameplates_Core.lua
+++ configs/WTF/Kui_Nameplates_Core.lua
@@ -12,16 +12,17 @@ KuiNameplatesCoreSaved = {
 			["nameonly_target"] = false,
 			["level_text"] = true,
 			["fade_avoid_mouseover"] = true,
-			["name_vertical_offset"] = -22,
+			["name_vertical_offset"] = -26,
 			["health_text_friend_dmg"] = 1,
 			["health_text"] = true,
+			["auras_sort"] = 1,
 			["fade_avoid_casting_hostile"] = true,
 			["fade_non_target_alpha"] = 0.3,
 			["bar_animation"] = 2,
 			["execute_enabled"] = false,
 			["nameonly_friends"] = false,
 			["bar_texture"] = "Smoother",
-			["auras_offset"] = 2,
+			["auras_offset"] = 4,
 			["colour_hated"] = {
 				1, -- [1]
 				0.2862745098039216, -- [2]
@@ -35,8 +36,11 @@ KuiNameplatesCoreSaved = {
 				1, -- [4]
 			},
 			["frame_width"] = 160,
+			["frame_glow_size_shadow"] = 5,
+			["bot_vertical_offset"] = -6,
 			["frame_height_target"] = 22,
 			["nameonly"] = false,
+			["frame_glow_size_target"] = 6,
 			["fade_avoid_nameonly"] = false,
 			["auras_centre"] = false,
 			["colour_neutral"] = {
@@ -44,19 +48,17 @@ KuiNameplatesCoreSaved = {
 				0.8, -- [2]
 				0, -- [3]
 			},
-			["frame_width_target"] = 180,
 			["frame_height"] = 18,
-			["bot_vertical_offset"] = -6,
-			["frame_glow_size_target"] = 6,
+			["fade_avoid_casting_interruptible"] = true,
+			["hide_names"] = false,
 			["frame_height_personal"] = 14,
 			["frame_padding_x"] = 0,
 			["nameonly_friendly_players"] = false,
 			["font_face"] = "聊天",
 			["fade_avoid_tracked"] = true,
-			["fade_avoid_casting_interruptible"] = true,
-			["frame_glow_size_shadow"] = 5,
-			["font_style"] = 4,
 			["fade_conditional_alpha"] = 0.1,
+			["font_style"] = 4,
+			["frame_width_target"] = 180,
 		},
 	},
 }
