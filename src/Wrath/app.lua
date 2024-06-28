6928ac569bd5d5dcbc6f538a7ef5e3e672447fa1
lozye <lozye@qq.com>
2024-06-28 11:27:59 +0800

    auto_sync

--- src/Wrath/app.lua
+++ src/Wrath/app.lua
@@ -1,6 +1,7 @@
 local App = {}
 do
     -- 代码主入口
+    -- https://github.com/Gethe/wow-ui-source/tree/3.4.3/Interface_Wrath/FrameXML
     App.Main = function()
         LPackage.Tooltip.Listen();
         LPackage.Chat.Listen();
