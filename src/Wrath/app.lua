9d33698c7985415a52d8028b65cc405429b2040c
lozye <lozye@qq.com>
2024-06-28 11:29:55 +0800

    auto_sync

--- src/Wrath/app.lua
+++ src/Wrath/app.lua
@@ -1,9 +1,17 @@
 local App = {}
 do
+
+    local CVarSetting=function()
+        -- 不显示小队框体 跨服服务器
+        C_CVar.SetCVar("commentatorLossOfControlTextNameplate", 0);        -- 
+        C_CVar.SetCVar('predictedHealth', 1)     
+    end
+
     -- 代码主入口
     -- https://github.com/Gethe/wow-ui-source/tree/3.4.3/Interface_Wrath/FrameXML
     App.Main = function()
-        LPackage.Tooltip.Listen();
+        CVarSetting();
+        -- LPackage.Tooltip.Listen();
         LPackage.Chat.Listen();
     end
 end
